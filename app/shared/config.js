eda21529b8a48f89a34360d2bc68af20f91dc3ce
Ryan Joseph Constantino <ryan.constantino93@gmail.com>
2016-09-03 06:29:25 -0700

    Removed automatically generated files from repo

--- app/shared/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var config = {
-    port: 3030,
-    hostname: window.location.hostname
-};
-exports.baseUrl = "http://" + config.hostname + ":" + config.port;
-//# sourceMappingURL=config.js.map
